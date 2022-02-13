4dd423246f77bd08db862decb651669b81c5677b
Danny Meier <development@danflash.com>
2022-02-13 19:49:18 +0100

    Update fan-mode-button-row.js

--- dist/fan-mode-button-row.js
+++ dist/fan-mode-button-row.js
@@ -1,8 +1,8 @@
 window.customCards = window.customCards || [];
 window.customCards.push({
-  type: "fan-mode-button-row",
-  name: "fan mode button row",
-  description: "A plugin to display your fan controls in a button row.",
+  type: "fan-mode-4-button-row",
+  name: "fan mode 4+1 button row",
+  description: "A plugin to display your fan controls in a 4+1 button row.",
   preview: false,
 });
 
@@ -46,6 +46,12 @@ class CustomFanModeRow extends Polymer.Element {
 								disabled='[[_midLeftState]]'>[[_midLeftText]]</button>
 							<button
 								class='mode'
+								style='[[_midColor]];min-width:[[_width]];max-width:[[_width]];height:[[_height]];[[_hideMid]]'
+								toggles name="[[_midName]]"
+								on-click='setMode'
+								disabled='[[_midState]]'>[[_midText]]</button>
+							<button
+								class='mode'
 								style='[[_midRightColor]];min-width:[[_width]];max-width:[[_width]];height:[[_height]];[[_hideMidRight]]'
 								toggles name="[[_midRightName]]"
 								on-click='setMode'
@@ -73,26 +79,32 @@ class CustomFanModeRow extends Polymer.Element {
 				_modeOne: String,
 				_modeTwo: String,
 				_modeThree: String,
+				_modeFour: String,
 				_width: String,
 				_height: String,
 				_leftColor: String,
 				_midLeftColor: String,
+				_midColor: String,
 				_midRightColor: String,
 				_rightColor: String,
 				_leftText: String,
 				_midLeftText: String,
+				_midText: String,
 				_midRightText: String,
 				_rightText: String,
 				_leftName: String,
 				_midLeftName: String,
+				_midName: String,
 				_midRightName: String,
 				_rightName: String,
 				_leftState: Boolean,
 				_midLeftState: Boolean,
+				_midState: Boolean,
 				_midRightState: Boolean,
 				_rightState: Boolean,
 				_hideMidLeft: Boolean,
-				_hideMidLeft: Boolean,
+				_hideMid: Boolean,
+				_hideMidRight: Boolean,
 		}
 	}
 
@@ -110,17 +122,20 @@ class CustomFanModeRow extends Polymer.Element {
 			modeOne: "low",
 			modeTwo: "medium",
 			modeThree: "high",
+			modeThree: "max",
 			width: '30px',
 			height: '30px',
 			isOffColor: '#f44c09',
 			isOnModeOneColor: '#43A047',
 			isOnModeTwoColor: '#43A047',
 			isOnModeThreeColor: '#43A047',
+			isOnModeFourColor: '#43A047',
 			buttonInactiveColor: '#759aaa',
 			customOffText: 'OFF',
 			customModeOneText: 'LOW',
 			customModeTwoText: 'MED',
 			customModeThreeText: 'HIGH',
+			customModeFourText: 'MAX',
 			...config
 		};
 	}
@@ -140,21 +155,25 @@ class CustomFanModeRow extends Polymer.Element {
 		const onM1Clr = config.isOnModeOneColor;
 		const onM2Clr = config.isOnModeTwoColor;
 		const onM3Clr = config.isOnModeThreeColor;
+		const onM4Clr = config.isOnModeFourColor;
 		const offClr = config.isOffColor;
 		const buttonOffClr = config.buttonInactiveColor;
 		const mOff = config.modeOff;
 		const m1 = config.modeOne;
 		const m2 = config.modeTwo;
 		const m3 = config.modeThree;
+		const m4 = config.modeFour;
 		const custOffTxt = config.customOffText;
 		const custM1Txt = config.customModeOneText;
 		const custM2Txt = config.customModeTwoText;
 		const custM3Txt = config.customModeThreeText;
+		const custM4Txt = config.customModeFourText;
 						
 		let offstate;
 		let mode1;
 		let mode2;
 		let mode3;
+		let mode4;
 		
 		if (custModes) {
 			if (stateObj && stateObj.attributes) {
@@ -164,6 +183,8 @@ class CustomFanModeRow extends Polymer.Element {
 					mode2 = 'on';
 				} else if (stateObj.state == 'on' && stateObj.attributes.preset_mode == m3 ) {
 					mode3 = 'on';
+				} else if (stateObj.state == 'on' && stateObj.attributes.preset_mode == m4 ) {
+					mode4 = 'on';
 				} else {
 					offstate = 'on';
 				}	
@@ -176,6 +197,8 @@ class CustomFanModeRow extends Polymer.Element {
 					mode2 = 'on';
 				} else if (stateObj.state == 'on' && stateObj.attributes.preset_mode == "high" ) {
 					mode3 = 'on';
+				} else if (stateObj.state == 'on' && stateObj.attributes.preset_mode == "max" ) {
+					mode4 = 'on';
 				} else {
 					offstate = 'on';
 				}	
@@ -186,27 +209,32 @@ class CustomFanModeRow extends Polymer.Element {
 		let m1text;
 		let m2text;
 		let m3text;
+		let m4text;
 		
 		if (custText) {
 			offtext = custOffTxt;
 			m1text = custM1Txt;
 			m2text = custM2Txt;
 			m3text = custM3Txt;
+			m4text = custM4Txt;
 		} else if (custModes) {
 			offtext = mOff;
 			m1text = m1;
 			m2text = m2;
 			m3text = m3;
+			m4text = m4;
 		} else {
 			offtext = "OFF";
 			m1text = "LOW";
 			m2text = "MED";
 			m3text = "HIGH";
+			m4text = "MAX";
 		}
 		
 		let mode1color;
 		let mode2color;
 		let mode3color;
+		let mode4color;
 		let offcolor;
 
 				
@@ -225,6 +253,11 @@ class CustomFanModeRow extends Polymer.Element {
 				mode3color = 'background-color:'  + onM3Clr;
 			} else {
 				mode3color = 'background-color:' + buttonOffClr;
+			}
+			if (mode4 == 'on') {
+				mode4color = 'background-color:'  + onM4Clr;
+			} else {
+				mode4color = 'background-color:' + buttonOffClr;
 			}
 			if (offstate == 'on') {
 				offcolor = 'background-color:'  + offClr;
@@ -247,6 +280,11 @@ class CustomFanModeRow extends Polymer.Element {
 			} else {
 				mode3color = 'background-color: var(--switch-unchecked-color)';
 			}
+			if (mode4 == 'on') {
+				mode4color = 'background-color: var(--switch-checked-color)';
+			} else {
+				mode4color = 'background-color: var(--switch-unchecked-color)';
+			}
 			if (offstate == 'on') {
 				offcolor = 'background-color: var(--switch-checked-color)';
 			} else {
@@ -255,8 +293,10 @@ class CustomFanModeRow extends Polymer.Element {
 		}
 		
 		let twomodes_left;
+		let twomodes_mid;
 		let twomodes_right;
 		
+		// Todo: For mid button
 		if (twoModes) {
 			if (revButtons) {
 				twomodes_right = 'display:none';
@@ -277,61 +317,74 @@ class CustomFanModeRow extends Polymer.Element {
 		let m1name = 'mode1'
 		let m2name = 'mode2'
 		let m3name = 'mode3'
+		let m4name = 'mode4'
 		
 		if (revButtons) {
 			this.setProperties({
 				_stateObj: stateObj,
 				_leftState: offstate == 'on',
 				_midLeftState: mode1 === 'on',
-				_midRightState: mode2 === 'on',
-				_rightState: mode3 === 'on',
+				_midState: mode2 === 'on',
+				_midRightState: mode3 === 'on',
+				_rightState: mode4 === 'on',
 				_width: buttonwidth,
 				_height: buttonheight,
 				_leftColor: offcolor,
 				_midLeftColor: mode1color,
-				_midRightColor: mode2color,
-				_rightColor: mode3color,
+				_midColor: mode2color,
+				_midRightColor: mode3color,
+				_rightColor: mode4color,
 				_modeOff: mOff,
 				_modeOne: m1,
 				_modeTwo: m2,
 				_modeThree: m3,
+				_modeFour: m4,
 				_leftText: offtext,
 				_midLeftText: m1text,
-				_midRightText: m2text,
-				_rightText: m3text,
+				_midText: m2text,
+				_midRightText: m3text,
+				_rightText: m4text,
 				_leftName: offname,
 				_midLeftName: m1name,
-				_midRightName: m2name,
-				_rightName: m3name,
+				_midName: m2name,
+				_midRightName: m3name,
+				_rightName: m4name,
 				_hideMidLeft: twomodes_left,
+				_hideMid: twomodes_mid,
 				_hideMidRight: twomodes_right,
 			});
 		} else {
 			this.setProperties({
 				_stateObj: stateObj,
-				_leftState: mode3 == 'on',
-				_midLeftState: mode2 === 'on',
+				_leftState: mode4 == 'on',
+				_midLeftState: mode3 === 'on',
+				_midState: mode2 === 'on',
 				_midRightState: mode1 === 'on',
 				_rightState: offstate === 'on',
 				_width: buttonwidth,
 				_height: buttonheight,
-				_leftColor: mode3color,
-				_midLeftColor: mode2color,
+				_leftColor: mode4color,
+				_midLeftColor: mode3color,
+				_midColor: mode2color,
 				_midRightColor: mode1color,
 				_rightColor: offcolor,
 				_modeOff: mOff,
 				_modeOne: m1,
 				_modeTwo: m2,
 				_modeThree: m3,
-				_leftText: m3text,
-				_midLeftText: m2text,
+				_modeFour: m4,
+				_leftText: m4text,
+				_midLeftText: m3text,
+				_midText: m2text,
 				_midRightText: m1text,
 				_rightText: offtext,
-				_leftName: m3name,
-				_midLeftName: m2name,
+				_leftName: m4name,
+				_midLeftName: m3name,
+				_midName: m2name,
 				_midRightName: m1name,
 				_rightName: offname,
 				_hideMidLeft: twomodes_left,
+				_hideMid: twomodes_mid,
 				_hideMidRight: twomodes_right,
 			});
 		}
@@ -361,8 +414,12 @@ class CustomFanModeRow extends Polymer.Element {
 				param.preset_mode = this._modeThree;
 				this.hass.callService('fan', 'set_preset_mode', param);
 			}
+			} else if (mode == 'mode4') {
+				param.preset_mode = this._modeFour;
+				this.hass.callService('fan', 'set_preset_mode', param);
+			}
 		}
 	}
 }
 	
-customElements.define('fan-mode-button-row', CustomFanModeRow);
+customElements.define('fan-mode-4-button-row', CustomFanModeRow);
